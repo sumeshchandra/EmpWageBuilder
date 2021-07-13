37837477ef32956984a75175fcec94b0f1c9db41
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-13 19:45:04 +0530

    Added: Part time wage included

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,7 @@
 public class EmpWage {
 
 	static final int IS_FULL_TIME =1;
+	static final int IS_PART_TIME =2;
 	static final int EMP_RATE_PER_HRS=20;
 	public static void main(String[] args) {
 	System.out.println("Hello guys, Welcome to wage computation ");
@@ -9,7 +10,9 @@ public class EmpWage {
 	double empCheck = Math.floor(Math.random() * 10)% 2;
 	if (empCheck == IS_FULL_TIME)
 	   empHrs=8;
- 	else
+ 	else if (empCheck == IS_PART_TIME)
+	   empHrs =4;
+	else
     	   empHrs=0;
 	empWage=empHrs * EMP_RATE_PER_HRS;
 	System.out.println("Emp Wage: " + empWage );
