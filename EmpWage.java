02af6b36d4aef0a1f9458b4feffb1bb1e39619e1
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-13 21:20:43 +0530

    Added: Calculated no of Working days

--- EmpWage.java
+++ EmpWage.java
@@ -3,22 +3,29 @@ public class EmpWage {
 	static final int IS_FULL_TIME =1;
 	static final int IS_PART_TIME =2;
 	static final int EMP_RATE_PER_HRS=20;
+	static final int NUM_OF_WORKING_DAYS=20;
 	public static void main(String[] args) {
 	System.out.println("Hello guys, Welcome to wage computation ");
 	int empHrs = 0;
 	int empWage = 0;
+	int totalEmpWage=0;
 	double empCheck = Math.floor(Math.random() * 10)% 2;
-	switch (empCheck){
-    		case IS_PART_TIME:
-    		empHrs=8;
-    		break;
-    		case IS_FULL_TIME:
-    		empHrs=8;
-    		break;
-    		default:
-    		empHrs=0;
-	    }
-	empWage=empHrs * EMP_RATE_PER_HRS;
-	System.out.println("Emp Wage: " + empWage );
+	 for (int day=0; day < NUM_OF_WORKING_DAYS ; day++ ) {
+     int empCheck = (int) Math.floor(Math.random()* 10) % 3;
+     switch (empCheck){
+       case IS_PART_TIME:
+          empHrs=8;
+          break;
+       case IS_FULL_TIME:
+          empHrs=8;
+          break;
+       default:
+         empHrs=0;
+    }
+ 	  empWage=empHrs * EMP_RATE_PER_HRS;
+ 	  totalEmpWage += empWage;
+	   System.out.println("Emp Wage: " + empWage );
+	  }
+	   System.out.println("Total Emp Wage: " + totalEmpWage );
 	 }
 }
