8e38cb783ce912f802d8396f05b277cfb754c871
sumesh chandra jena <69643666+sumeshchandra@users.noreply.github.com>
2021-07-13 19:18:46 +0530

    Resolved

--- EmpWage.java
+++ EmpWage.java
@@ -3,8 +3,6 @@ public class EmpWage {
         int IS_FULL_TIME = 1;
 	public static void main(String[] args) {
 	
-	int IS_FULL_TIME = 1;
-	
 	double empCheck = Math.floor(Math.random() * 10)% 2;
 	if (empCheck == IS_FULL_TIME)
 		System.out.println("Employee is present");
